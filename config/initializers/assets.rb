201a989e51dd4c5e711e27904cc43227327ec038
PennyWeiser1 <weiserthanyou3@gmail.com>
2024-09-27 19:19:51 +0000

    bootstrap is working as intended

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,4 +12,3 @@ Rails.application.config.assets.version = "1.0"
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
-Rails.application.config.assets.precompile += %w( application.scss )
